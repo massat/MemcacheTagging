0e01c297e4f11dc3e3369cfed8e146a909c136bb
Masato Hirai <mail@massat.jp>
2010-03-02 21:32:10 +0900

    add tests

--- test/unit.php
+++ test/unit.php
@@ -4,12 +4,46 @@ require dirname(__FILE__)          . '/lib/lime/lib/lime.php';
 require dirname(dirname(__FILE__)) . '/MemcacheTagging.class.php';
 
 $lime  = new lime_test(null, new lime_output_color());
-$cache = MemcacheTagging::getInstance();
 
 ////////////////////////////////////////////////////
+// TESTS
+////////////////////////////////////////////////////
 
+/*
+ * connect to server
+ */
+$cache = MemcacheTagging::getInstance();
+
+$lime->diag('test instantiate');
+$lime->ok($cache, 'connect to memcache server without options');
+
+$options = array('servers' => array(
+    array(
+        'host' => 'localhost',
+        'port' => 11211,
+    )
+));
+$cache = MemcacheTagging::getInstance($options);
+$lime->ok($cache, 'connect to memcache server with options');
+
+
+$options = array('servers' => array(
+    array(
+        'host' => 'localhost',
+        'port' => 11211,
+    ),
+    array(
+        'host' => 'localhost',
+        'port' => 11211,
+    ),
+));
+$cache = MemcacheTagging::getInstance($options);
+$lime->ok($cache, 'connect to multi memcache servers with options');
+
+/**
+ * get()
+ */
 $cache->flush();
-
 $lime->diag('test get()');
 
 $lime->ok($cache->get('hoge') === null, 'returns "null" if no value');
@@ -75,7 +109,7 @@ foreach($datas as $data) {
 
 $cache->flush();
 
-$lime->diag('test set() -> getByTags()');
+$lime->diag('test set() -> getByTag()');
 
 foreach($datas as $data) {
     $key   = $data['key'];
@@ -86,9 +120,96 @@ foreach($datas as $data) {
 }
 
 $values = $cache->getByTag(1);
-$diff   = array_diff($values, array('value_1'));
-$lime->ok(empty($diff), sprintf('values for "%s" is "(%s)"', 1, join(',', $values)));
+$diff1  = array_diff($values, array('value_1'));
+$diff2  = array_diff(array('value_1'), $values);
+$lime->ok(empty($diff1) && empty($diff2), sprintf('values for "%s" is "(%s)"', 1, join(',', $values)));
 
 $values = $cache->getByTag(2);
-$diff   = array_diff($values, array('value_1', 'value_2'));
-$lime->ok(empty($diff), sprintf('values for "%s" is "(%s)"', 2, join(',', $values)));
+$diff1   = array_diff($values, array('value_1', 'value_2'));
+$diff2   = array_diff(array('value_1', 'value_2'), $values);
+$lime->ok(empty($diff1) && empty($diff2), sprintf('values for "%s" is "(%s)"', 2, join(',', $values)));
+
+////////////////////////////////////////////////////
+
+$cache->flush();
+
+$lime->diag('test set() -> getTags()');
+foreach($datas as $data) {
+    $key   = $data['key'];
+    $value = $data['value'];
+    $tags  = $data['tags'];
+    
+    $cache->set($key, $value, $tags);
+
+    $gotTags = $cache->getTags($key);
+    
+    $diff1 = array_diff($gotTags, $tags);
+    $diff2 = array_diff($tags, $gotTags);
+    $lime->ok(empty($diff1) && empty($diff2), sprintf('tags for "%s" is "(%s)"', $key, join(',', $tags)));
+}
+
+////////////////////////////////////////////////////
+
+$cache->flush();
+
+$lime->diag('test set() -> delete() -> getByTag()');
+foreach($datas as $data) {
+    $key   = $data['key'];
+    $value = $data['value'];
+    $tags  = $data['tags'];
+    
+    $cache->set($key, $value, $tags);
+}
+
+$cache->delete('key1');
+
+$values = $cache->getByTag('1');
+$lime->ok(empty($values), 'values for tag "1" is "emptry"');
+
+$values = $cache->getByTag('2');
+$diff1  = array_diff($values, array('value_2'));
+$diff2  = array_diff(array('value_2'), $values);
+$lime->ok(empty($diff1) && empty($diff2), 'values for tag "2" is "(value_2)"');
+
+$values = $cache->getByTag('3');
+$diff1  = array_diff($values, array('value_2'));
+$diff2  = array_diff(array('value_2'), $values);
+$lime->ok(empty($diff1) && empty($diff2), 'values for tag "3" is "(value_2)"');
+
+$values = $cache->getByTag('4');
+$diff1  = array_diff($values, array('value_2'));
+$diff2  = array_diff(array('value_2'), $values);
+$lime->ok(empty($diff1) && empty($diff2), 'values for tag "4" is "(value_2)"');
+
+////////////////////////////////////////////////////
+
+$cache->flush();
+$lime->diag('test set() -> deleteByTag() -> get()');
+foreach($datas as $data) {
+    $key   = $data['key'];
+    $value = $data['value'];
+    $tags  = $data['tags'];
+    
+    $cache->set($key, $value, $tags);
+}
+
+$cache->deleteByTag('1');
+$v1 = $cache->get('key1');
+$v2 = $cache->get('key2');
+$lime->ok(is_null($v1), '"key1" is deleted');
+$lime->ok(!is_null($v2), '"key2" is not deleted');
+
+$cache->flush();
+foreach($datas as $data) {
+    $key   = $data['key'];
+    $value = $data['value'];
+    $tags  = $data['tags'];
+    
+    $cache->set($key, $value, $tags);
+}
+
+$cache->deleteByTag('2');
+$v1 = $cache->get('key1');
+$v2 = $cache->get('key2');
+$lime->ok(is_null($v1), '"key1" is deleted');
+$lime->ok(is_null($v2), '"key2" is deleted');
